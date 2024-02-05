c59335d10bd6047b655726009c79dfc4ce7e8c4b
Dario <dariopersonal16@gmail.com>
2024-02-05 10:24:47 +0100

    default router

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth } from '@/lib/auth.js'
 
 const router = createRouter({
-  history: createWebHistory('/IoT/'),
-  mode: 'hash',
+  history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'history',
   routes: [
     {
       path: '/',
